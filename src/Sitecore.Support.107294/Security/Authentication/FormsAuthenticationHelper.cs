1b0b183bb30fc6bdf4fe10f93c9380428b7f3c69
Oleg Burov <oleg.burov@outlook.com>
2016-04-19 12:20:04 -0700

    Implemented Sitecore Patch #107294.
    Rolled back fixes #365855 and #441004 with Forms Authentication Ticket
    and Cookie.

---
+++ src/Sitecore.Support.107294/Security/Authentication/FormsAuthenticationHelper.cs
@@ -0,0 +1,26 @@
+﻿using Sitecore.Security.Authentication;
+using Sitecore.Diagnostics;
+using Sitecore.Configuration;
+
+namespace Sitecore.Support.Security.Authentication
+{
+  class FormsAuthenticationHelper : Sitecore.Security.Authentication.FormsAuthenticationHelper
+  {
+    public FormsAuthenticationHelper(AuthenticationProvider provider) : base(provider)
+    {
+    }
+
+    public override string GetAuthenticationData(string key)
+    {
+      Assert.ArgumentNotNull(key, "key");
+      return (ClientContext.GetValue(key) as string);
+    }
+
+    public override void SetAuthenticationData(string key, string authenticationData)
+    {
+      Assert.ArgumentNotNull(key, "key");
+      Assert.ArgumentNotNull(authenticationData, "authenticationData");
+      ClientContext.SetValue(key, authenticationData);
+    }
+  }
+}
---
+++ src/Sitecore.Support.107294/Security/Authentication/FormsAuthenticationProvider.cs
@@ -0,0 +1,29 @@
+﻿using Sitecore.Diagnostics;
+using System.Collections.Specialized;
+
+namespace Sitecore.Support.Security.Authentication
+{
+  class FormsAuthenticationProvider : Sitecore.Security.Authentication.FormsAuthenticationProvider
+  {
+    private FormsAuthenticationHelper helper;
+
+    public override void Initialize(string name, NameValueCollection config)
+    {
+      base.Initialize(name, config);
+      this.helper = new FormsAuthenticationHelper(this);
+    }
+
+    public override string GetAuthenticationData(string key)
+    {
+      Assert.ArgumentNotNull(key, "key");
+      return this.helper.GetAuthenticationData(key);
+    }
+
+    public override void SetAuthenticationData(string key, string authenticationData)
+    {
+      Assert.ArgumentNotNull(key, "key");
+      Assert.ArgumentNotNull(authenticationData, "authenticationData");
+      this.helper.SetAuthenticationData(key, authenticationData);
+    }
+  }
+}
